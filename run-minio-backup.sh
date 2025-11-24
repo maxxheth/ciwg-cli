c29e9ea5c35bb79ea6947d271f035a85b18682be
Maximillian Heth <info@edfx.co>
2025-11-24 17:06:33 -0600

    Update backup command to adjust remainder parameter and include capacity limit respect

--- run-minio-backup.sh
+++ run-minio-backup.sh
@@ -12,6 +12,6 @@ HOSTNAME="$(hostname)"
 LOGDIR="/root/logs"
 mkdir -p "$LOGDIR"
 
-ciwg-cli backup create "$HOSTNAME" --prune --remainder 5 --local --capacity-threshold 80 >> "$LOGDIR/minio-app-backup.log" 2>&1 &
+ciwg-cli backup create "$HOSTNAME" --prune --remainder 4 --local --capacity-threshold 80 --respect-capacity-limit >> "$LOGDIR/minio-app-backup.log" 2>&1 &
 
 exit 0
